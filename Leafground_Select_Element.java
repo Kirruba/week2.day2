8356516b416a4ee5fb6af703de6b72bb420a270c
Kirruba <112795574+Kirruba@users.noreply.github.com>
2022-09-22 23:26:15 +0530

    Add files via upload

--- Leafground_Select_Element.java
+++ Leafground_Select_Element.java
@@ -18,6 +18,7 @@ public class Leafground_Select_Element {
 		driver.get("https://leafground.com/select.xhtml");
 		driver.manage().window().maximize();
 
+		// Add Implicit waits
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
 		// Which is your favorite UI Automation tool?
@@ -26,23 +27,40 @@ public class Leafground_Select_Element {
 		dd.selectByVisibleText("Selenium");
 
 		// Choose your preferred country
-		driver.findElement(By.xpath("//div[@id='j_idt87:country']")).click();
-		driver.findElement(By.xpath("//ul[@id='j_idt87:country_items']/li[text()='India']")).click();
+		WebElement findElement1 = driver.findElement(By.xpath("//label[@id='j_idt87:country_label']"));
+		findElement1.click();
+		driver.findElement(By.xpath("//li[text()='India']")).click();
 
 		// Confirm Cities belongs to Country is loaded
 		Thread.sleep(5000);
-		driver.findElement(By.xpath("//div[@id='j_idt87:city']")).click();
-		driver.findElement(By.xpath("//ul[@id='j_idt87:city_items']/li[text()='Chennai']")).click();
+		WebElement findElement2 = driver.findElement(By.xpath("//label[@id='j_idt87:city_label']"));
+		findElement2.click();
+		WebElement city = driver.findElement(By.xpath("//li[@id='j_idt87:city_2']"));
+		String text1 = city.getText();
+		System.out.println(text1);
+		city.click();
+
+		// Change the Country & check if City is changed
+		Thread.sleep(5000);
+		findElement1.click();
+		driver.findElement(By.xpath("//li[text()='Germany']")).click();
+
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//label[@id='j_idt87:city_label']")).click();
+		WebElement city1 = driver.findElement(By.xpath("//li[@id='j_idt87:city_1']"));
+		String text2 = city1.getText();
+		System.out.println(text2);
+		city1.click();
 
 		// Choose the Course
 		driver.findElement(
 				By.xpath("//span[@class='ui-button-icon-primary ui-icon ui-icon-triangle-1-s']/parent::button"))
 				.click();
 		driver.findElement(By.xpath("//li[text()='Selenium WebDriver']")).click();
-		WebElement findElement1 = driver.findElement(By.xpath("//li[text()='Playwright']"));
-		driver.executeScript("arguments[0].click();", findElement1);
-		WebElement findElement2 = driver.findElement(By.xpath("//li[text()='PostMan']"));
-		driver.executeScript("arguments[0].click();", findElement2);
+		WebElement findElement3 = driver.findElement(By.xpath("//li[text()='Playwright']"));
+		driver.executeScript("arguments[0].click();", findElement3);
+		WebElement findElement4 = driver.findElement(By.xpath("//li[text()='PostMan']"));
+		driver.executeScript("arguments[0].click();", findElement4);
 
 		// Choose language randomly
 		driver.findElement(By.xpath("//div[@id='j_idt87:lang']")).click();
